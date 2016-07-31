84315c07c08c7f8d29bdfc227a4268cba579388b
ingeusac <hildaeunicer@gmail.com>
2016-07-31 21:51:51 +0000

    Falta agregar las redirecciones

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,14 +11,36 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
-    return view('tasks');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
 });
 /**
  * Add New Task
  */
 Route::post('/task', function (Request $request) {
     //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('');
 });
 
 /**
@@ -26,4 +48,7 @@ Route::post('/task', function (Request $request) {
  */
 Route::delete('/task/{task}', function (Task $task) {
     //
+     $task->delete();
+
+    return redirect('');
 });
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -33,6 +33,53 @@
             </div>
         </form>
     </div>
+    <!-- todo -->
+    
+    <!-- Listado de tasks-->
+@if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
+            </div>
 
-    <!-- TODO: Current Tasks -->
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- Table Headings -->
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- Table Body -->
+                    <tbody>
+                        @foreach ($tasks as $task)
+                            <tr>
+                                <!-- Task Name -->
+                                <td class="table-text">
+                                    <div>{{ $task->name }}</div>
+                                </td>
+
+                                <td>
+                                    <!-- TODO: Delete Button -->
+                                        <!-- Delete Button -->
+                                        <td>
+                                            <form action="{{ url('task/'.$task->id) }}" method="POST">
+                                                {{ csrf_field() }}
+                                                {{ method_field('DELETE') }}
+                                    
+                                                <button type="submit" class="btn btn-danger">
+                                                    <i class="fa fa-trash"></i> Delete
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
